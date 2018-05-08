f1afced3fe08bebd2dbae36accc8075b30b384ae
Lauren Hamel <lauren.hamel@outlook.com>
2018-05-08 15:07:38 -0400

    Starts adding commands

--- msynq.js
+++ msynq.js
@@ -1,19 +1,91 @@
 #!/usr/bin/env Node
 
-const program = require('commander'),
-      chalk = require('chalk'),
-      package = require('package.json'),
-      config = require('data/config.json');
-      
+// Load configurations.
+let config = require('./data/config.json');
 
-program 
-  .version(package.version)
-  .command('storage [storage]', 'sets the storage location')
-  .command('config', 'opens the Msynq config file for editing')
-  .command('list', 'lists all sync configurations in the config file', {isDefault: true})
-  .command('sync', 'syncs all storage files with your system')
-  .command('unsync', 'unsyncs all storage files with your system')
-  .command('add [name] [src] [dest]', 'adds a new file or folder to storage')
-  .command('remove [name]', 'removes a file or folder from storage')
-  .command('restore', 'restores the default Msynq configurations')
-  .parse(process.argv);
+// Load dependencies.
+const program   = require('commander');
+const package   = require('./package.json');
+const commands  = require('./lib/commands.js')();
+const logger    = require('./lib/logger.js')();
+
+// Load utilities.
+require('./lib/prototypes.js');
+      
+// Set the program version.
+program .version(package.version);
+
+// Create `storage` command.
+program
+  .command('storage [storage]')
+  .description('sets the storage location')
+  .action(commands.storage);
+
+// Create `config` command.
+program
+  .command('config')
+  .description('opens the Msynq config file for editing')
+  .action(commands.config);
+
+// Create `list` command.
+program
+  .command('list')
+  .description('lists all sync configurations in the config file')
+  .action(commands.list);
+
+// Create `sync` command.
+program
+  .command('sync')
+  .description('syncs all storage files with your system')
+  .action(commands.sync);
+
+// Create `unsync` command.
+program
+  .command('unsync')
+  .description('unsyncs all storage files with your system')
+  .action(commands.unsync);
+
+// Create `add` command.
+program
+  .command('add [name] [src] [dest]')
+  .description('adds a new file or folder to storage')
+  .action(commands.add);
+
+// Create `remove` command.
+program
+  .command('remove [name]')
+  .description('removes a file or folder from storage')
+  .action(commands.remove);
+
+// Create `restore` command.
+program
+  .command('restore')
+  .description('restores the default Msynq configurations')
+  .action(commands.restore);
+  
+// Start the program.
+program.parse(process.argv);
+
+// Capture arguments.
+const ARGV = process.argv.slice(2);
+
+// Catch program errors.
+const ERROR = {
+  COMMAND: {
+    UNKNOWN: ARGV.length > 0 && Object.keys(commands).intersection(ARGV).length === 0,
+    NONEXISTENT: ARGV.length === 0
+  }
+};
+
+if( ERROR.COMMAND.UNKNOWN ) {
+  
+  logger.error('An unknown command was given.'); 
+  
+  program.help();
+  
+}
+if( ERROR.COMMAND.NONEXISTENT ) {
+  
+  program.help();
+  
+}
